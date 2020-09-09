d959c43ae0e45029fffe3eefc64df924ae7c7b2a
hamdy <hamdy.a.farag@gmail.com>
2020-09-09 11:49:46 +0200

    updated log messages

--- src/CrystalTools/gitrepo.cr
+++ src/CrystalTools/gitrepo.cr
@@ -27,12 +27,12 @@ module CrystalTools
     end
 
     def add(reponame, repo)
-      CrystalTools.log "Adding repo #{reponame} to cache", 2
+      CrystalTools.log "-  Caching repo (#{reponame})", 2
       @@redis.hset("gitrepos::repos", reponame, repo)
     end
 
     def remove(reponame)
-      CrystalTools.log "Removing repo #{reponame} to cache", 2
+      CrystalTools.log "-  Remove repo (#{reponame}) from Cache", 2
       @@redis.hdel("gitrepos::repos", reponame)
     end
 
